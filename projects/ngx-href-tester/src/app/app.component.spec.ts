dfe49e8a6503961e5811bbdaf382c0289494524b
Raphaël Balet <raphael@megaphone.info>
2024-02-07 13:58:56 +0100

    feat - testing

--- projects/ngx-href-tester/src/app/app.component.spec.ts
+++ projects/ngx-href-tester/src/app/app.component.spec.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common'
-import { TestBed, waitForAsync } from '@angular/core/testing'
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
 import { NgxHrefModule } from '../../../ngx-href/src/lib/href.module'
+import { NgxHrefService } from '../../../ngx-href/src/public-api'
 import { AppComponent } from './app.component'
+import { routes } from './app.module'
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let hrefService: NgxHrefService
+
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterModule.forRoot(routes),
+
         BrowserAnimationsModule,
         CommonModule,
-        AppComponent,
 
         // Vendors
         NgxHrefModule.forRoot({
@@ -18,12 +25,17 @@ describe('AppComponent', () => {
         }),
         ,
       ],
+      declarations: [AppComponent],
     }).compileComponents()
   }))
 
+  hrefService = TestBed.inject(NgxHrefService)
+
+  fixture = TestBed.createComponent(AppComponent)
+  fixture.detectChanges() // Initial binding
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    const app = fixture.debugElement.componentInstance
-    expect(app).toBeTruthy()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('h1')?.textContent).toContain('href tester is running!')
   })
 })
---
+++ projects/ngx-href/src/lib/tests/href.directive.spec.ts
@@ -0,0 +1,51 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { NgxHrefDirective } from 'ngx-href'
+import { NgxHrefModule } from '../href.module'
+import { NgxHrefService } from '../href.service'
+
+@Component({
+  template: `<button #testButton href="https://github.com">Internal link</button>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgxHrefDirective],
+})
+class HrefTestComponent {
+  viewContainerRef: ViewContainerRef
+  @ViewChild('testButton', { static: false }) testButton!: ElementRef
+
+  constructor(viewContainerRef: ViewContainerRef) {
+    this.viewContainerRef = viewContainerRef
+  }
+}
+
+describe('HrefDirective', () => {
+  let fixture: ComponentFixture<HrefTestComponent>
+  let hrefService: NgxHrefService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        NgxHrefModule.forRoot({
+          defaultTargetAttr: '_blank',
+        }),
+      ],
+      declarations: [HrefTestComponent],
+    })
+
+    hrefService = TestBed.inject(NgxHrefService)
+
+    fixture = TestBed.createComponent(HrefTestComponent)
+    fixture.detectChanges() // Initial binding
+  })
+
+  it('Component does exists', () => {
+    // @ToDo Add meaningful test
+    expect(fixture.componentInstance.testButton.nativeElement).toBeTruthy()
+  })
+})
